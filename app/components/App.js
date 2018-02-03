648414f13dc42cd471050ef1425ce2d642b20d00
hayden7913 <hayden321@gmail.com>
2018-02-02 23:04:00 -0800

    Adds App component

--- app/components/App.js
+++ app/components/App.js
@@ -1,16 +1,69 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import '../styles/footer.scss';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { hashHistory } from 'react-router';
+import Notification  from 'react-web-notification';
 import Routes from '../routes';
 
-const App = () =>
-    <div>
-        <h1>my new change</h1>
-        { Routes }
-        <footer className="footer">
-            <Link to="/">Filterable Table</Link>
-            <Link to="/about">About</Link>
-        </footer>
-    </div>;
+import { routeToProjectsPage, routeToTimerPage } from '../helpers/route';
+import { changeActiveLink, fetchProjects, handleKeyDown, toggleProjectNagModal } from '../actions/indexActions';
 
-export default App;
+import Nav from '../components/Nav';
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      showNotification: true
+    };
+  }
+
+  componentDidMount() {
+    const { fetchProjects, handleKeyDown } = this.props;
+
+    document.onkeydown = handleKeyDown;
+    fetchProjects();
+  }
+
+  render() {
+    const { isDesktopNotificationActive, location } = this.props;
+    const pathName = location.pathname;
+    const isProjectRoute = /projects/.test(pathName);
+
+    return (
+      <div>
+        <Nav
+          activeLink={isProjectRoute ? 'PROJECTS' : 'TIMER'}
+          handleTimerLinkClick={routeToTimerPage}
+          handleProjectsLinkClck={routeToProjectsPage}
+          isProjectRoute={isProjectRoute}
+        />
+        {Routes}
+        {isDesktopNotificationActive
+          && <Notification
+            title="Time's Up!"
+            ignore={false}
+            options={{ icon: 'images/tomato-timer.png' }}
+          />}
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  const { projects, timer } = state;
+  const { isDesktopNotificationActive } = timer;
+
+  return {
+    isDesktopNotificationActive,
+    projects: projects.items
+  };
+};
+
+export default connect(mapStateToProps, {
+  changeActiveLink,
+  fetchProjects,
+  handleKeyDown,
+  toggleProjectNagModal
+}
+)(App);
--- app/routes.js
+++ app/routes.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-import App from './containers/App';
+
 import AddProjectPage from './containers/AddProjectPage';
 import EditProjectPage  from './containers/EditProjectPage';
 import ProjectsPage from './containers/ProjectsPage';
